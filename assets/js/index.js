3b5ff6e58b2d75018de1b1d70e16405d75a44fdc
Kaull Developer <83796306+Kaullony@users.noreply.github.com>
2022-08-09 23:47:12 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+/************* NAVIGATION BAR **************/
+window.addEventListener("scroll", function () {
+
+  var header = document.querySelector("nav");
+  header.classList.toggle("menu-scroll", window.scrollY > 0)
+})
+
+
+/************* MENU RESPONSIVE **************/
+
+let menu = document.getElementById('menuEvent')
+
+function abrirMenu(){
+
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "flex"
+  fecharMenu.style.display = "flex"
+  abrirMenu.style.display = "none"
+}
+
+function fecharMenu(){
+
+  
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "none"
+  abrirMenu.style.display = "flex"
+  fecharMenu.style.display = "none"
+}
+
+function linkFechar(){
+
+  
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "none";
+  abrirMenu.style.display = "flex"
+  fecharMenu.style.display = "none"
+}
