6563ae17bd2dca7e7781118f6668d79c356e9c48
Kaull Developer <83796306+Kaullony@users.noreply.github.com>
2022-08-28 22:03:44 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,65 @@
 /************* NAVIGATION BAR **************/
 window.addEventListener("scroll", function () {
 
-    var header = document.querySelector("nav");
-    header.classList.toggle("menu-scroll", window.scrollY > 0)
-  })
-  
-  
-  /************* MENU RESPONSIVE **************/
-  
-  let menu = document.getElementById('menuEvent')
-  
-  function abrirMenu(){
-  
-    var fecharMenu = document.getElementById('fecharBtn')
-    var abrirMenu = document.getElementById('abrirBtn')
-  
-    menu.style.display = "flex"
-    fecharMenu.style.display = "flex"
-    abrirMenu.style.display = "none"
-  }
-  
-  function fecharMenu(){
-  
-    
-    var fecharMenu = document.getElementById('fecharBtn')
-    var abrirMenu = document.getElementById('abrirBtn')
-  
-    menu.style.display = "none"
-    abrirMenu.style.display = "flex"
-    fecharMenu.style.display = "none"
-  }
-  
-  function linkFechar(){
-  
-    
-    var fecharMenu = document.getElementById('fecharBtn')
-    var abrirMenu = document.getElementById('abrirBtn')
-  
-    menu.style.display = "none";
-    abrirMenu.style.display = "flex"
-    fecharMenu.style.display = "none"
-  }
-  
-  function openTopics(){
-    
-    var openTopics = document.getElementById('topics')
-    openTopics.style.display = "flex"
-  }
-  
-  function exitTopics(){
-  
-    var openTopics = document.getElementById('topics')
-    
-    if (!openTopics.contains(e.target)) {
-      openTopics.style.display = 'none';
-    }
-  }
+  var header = document.querySelector("nav");
+  header.classList.toggle("menu-scroll", window.scrollY > 0)
+})
 
 
-  // carrossel slick js
+/************* MENU RESPONSIVE **************/
 
- // CARROSSEL PRODUTOS EM DESTAQUES
+let menu = document.getElementById('menuEvent')
 
-$('.carrone').slick({
-  prevArrow: '.slick-prev-one',
-  nextArrow: '.slick-next-one',
+function abrirMenu(){
+
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "flex"
+  fecharMenu.style.display = "flex"
+  abrirMenu.style.display = "none"
+}
+
+function fecharMenu(){
+
+  
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "none"
+  abrirMenu.style.display = "flex"
+  fecharMenu.style.display = "none"
+}
+
+function linkFechar(){
+
+  
+  var fecharMenu = document.getElementById('fecharBtn')
+  var abrirMenu = document.getElementById('abrirBtn')
+
+  menu.style.display = "none";
+  abrirMenu.style.display = "flex"
+  fecharMenu.style.display = "none"
+}
+
+function openTopics(){
+  
+  var openTopics = document.getElementById('topics')
+  openTopics.style.display = "flex"
+}
+
+
+// CARROSSEL PRODUTOS EM DESTAQUES
+
+$('.carrossel').slick({
+  // prevArrow: '.slick-prev-one',
+  // nextArrow: '.slick-next-one',
   autoplay: true,
+  arrows: false,
   autoplaySpeed: 4000,
   infinite: true,
   speed: 300,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
   responsive: [
     {
@@ -96,3 +86,16 @@ $('.carrone').slick({
     }
   ]
 });
+
+// MODALS
+
+const content = document.getElementById('content')
+const closeBtnModal = document.getElementsByClassName('btn-close-modal')
+
+function openModal(){
+  content.style.display = "flex"
+}
+
+function fecharmodal(){
+  content.style.display = "none"
+}
