327f9ae9d2c92b6972ff76987399a01f362864ca
Kaull Developer <83796306+Kaullony@users.noreply.github.com>
2022-10-07 00:47:52 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,7 +56,7 @@ $('.carrossel-categ').slick({
   prevArrow: '.slick-prev-two',
   nextArrow: '.slick-next-two',
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 5000,
   infinite: true,
   speed: 300,
   slidesToShow: 4,
